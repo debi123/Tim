257aae89b8a5364bed20e677de4e08e3b15bad7e
debi123 <debarati.sinharoy@emc.com>
2017-11-28 13:54:53 +0530

    Update run_test.sh

--- proj2/run_test.sh
+++ proj2/run_test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-spark-submit  --packages com.hortonworks:shc-core:1.1.1-2.1-s_2.11 --repositories http://repo.hortonworks.com/content/groups/public/ test_r.py
+# spark-submit  --packages com.hortonworks:shc-core:1.1.1-2.1-s_2.11 --repositories http://repo.hortonworks.com/content/groups/public/ test_r.py
 
 python3 test_file.py
