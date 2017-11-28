c92861f19679df8fcf4e6fd1b50cdd684a5e21a2
debi123 <debarati.sinharoy@emc.com>
2017-11-28 14:36:52 +0530

    Update test_file.py

--- proj2/test_file.py
+++ proj2/test_file.py
@@ -66,8 +66,8 @@ class TextAnalysisTests(unittest.TestCase):
 
     def test_no_deletion(self):
         """Check that the function doesn't delete the input file."""
-        analyze_text(self.filename)
-        self.assertTrue(os.path.exists('./trial.csv))
+        analyze_text('./trial.csv')
+        self.assertTrue(os.path.exists(self.filename))
 
 
 if __name__ == '__main__':
